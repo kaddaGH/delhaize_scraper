929f096030b99fd261bd3079f6adfebce68df165
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-11 12:55:18 +0100

    Update scraper.

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -11,13 +11,13 @@ require 'cgi'
 
 
 }
-search_terms = ['Red Bull', 'RedBull', 'Energidryck', 'Energidrycker']
+search_terms = ['Red Bull', 'RedBull', 'Energy Drink', 'Boisson Énergisante']
 search_terms.each do |search_term|
 
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://www.delhaize.be/search/products/loadMore?text=red+bull&pageSize=20&pageNumber=0&sort=relevance&q=#{CGI.escape(search_term)}%3Arelevance",
+      url: "https://www.delhaize.be/search/products/loadMore?text=#{CGI.escape(search_term)}&pageSize=20&pageNumber=0&sort=relevance&q=#{CGI.escape(search_term)}%3Arelevance",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
