1948aad899780c2dd372c7748ffb41fdad4c9287
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-11 15:37:22 +0100

    update scraper

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,7 +1,20 @@
 require 'cgi'
 require './lib/headers'
 
-search_terms = ['Boisson Energisante']
+
+
+pages << {
+    page_type: 'products_search',
+    method: 'GET',
+    headers: ReqHeaders::SEARCH_PAGE_HEADER_REQ,
+    url: "https://www.delhaize.be/fr-be/shop/Boissons-et-alcools/Boissons-energisantes/c/v2DRIENE/loadMore?q=%3Apopularity&sort=popularity&pageSize=12&pageNumber=0",
+    vars: {
+        'input_type' => 'taxonomy',
+        'search_term' => '-',
+        'page' => 1
+    }
+}
+search_terms = ['Red Bull', 'RedBull', 'Energy Drink', 'Boisson Energisante']
 search_terms.take(1).each do |search_term|
 
   pages << {
