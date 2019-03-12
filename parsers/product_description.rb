7e90db6765c14ca9060bd9422f64c48a84214e86
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-03-12 10:52:08 +0100

    update scraper.

--- parsers/product_description.rb
+++ parsers/product_description.rb
@@ -1,10 +1,10 @@
 body = Nokogiri.HTML(content)
-description = body.at_css(".ShowMoreLess__content").to_s rescue ''
+description = body.at_css(".ShowMoreLess__content").to_s rescue ""
 
 
 description = description.gsub(/^[^{]+<h3>Description<\/h3>/,'').gsub(/<h3>[^{]+\Z/,'').gsub(/<[^<>]+>/,'').gsub(/[\n\s,]+/,' ').strip
 
-description_second=body.at_css(".col-sm-30:contains('Description')").text.gsub(/Description/,'').gsub(/[\n\s,]+/,' ').strip rescue ()
+description_second=body.at_css(".col-sm-30:contains('Description')").text.gsub(/Description/,'').gsub(/[\n\s,]+/,' ').strip rescue ""
 
 
 products_details = page['vars']['product_details']
